a3ca72e6c48600deb936a755483ceaa64e18d76b
1000JI <chjh1992@gmail.com>
2020-06-09 16:18:59 +0900

    feat: Login View, SignUp View Setup

--- FireChat/FireChat/Controller/Authentication/LoginController.swift
+++ FireChat/FireChat/Controller/Authentication/LoginController.swift
@@ -19,35 +19,47 @@ class LoginController: UIViewController {
         return iv
     }()
     
-    private let emailContainerView: UIView = {
-        let containerView = UIView()
-        containerView.backgroundColor = .cyan
-        
-        let iv = UIImageView()
-        iv.image = UIImage(systemName: "envelope")
-        iv.tintColor = .white
-        
-        containerView.addSubview(iv)
-        iv.centerY(inView: containerView)
-        
-        containerView.setHeight(height: 50)
+    private lazy var emailContainerView: UIView = {
+        let containerView = InputContainerView(image: #imageLiteral(resourceName: "ic_mail_outline_white_2x"),
+                                               textField: emailTextField)
         return containerView
     }()
     
-    private let passwordContainerView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .yellow
-        view.setHeight(height: 50)
-        return view
+    private lazy var passwordContainerView: InputContainerView = {
+        let containerView = InputContainerView(image: #imageLiteral(resourceName: "ic_lock_outline_white_2x"),
+                                               textField: passwordTextField)
+        return containerView
     }()
     
     private let loginButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Log In", for: .normal)
         button.layer.cornerRadius = 5
-        button.titleLabel?.font = .boldSystemFont(ofSize: 16)
-        button.backgroundColor = .systemRed
+        button.titleLabel?.font = .boldSystemFont(ofSize: 18)
+        button.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+        button.setTitleColor(.white, for: .normal)
         button.setHeight(height: 50)
+        return button
+    }()
+    
+    private let emailTextField = CustomTextField(placeholder: "Email")
+    
+    private let passwordTextField: CustomTextField = {
+        let tf = CustomTextField(placeholder: "password")
+        tf.isSecureTextEntry = true
+        return tf
+    }()
+    
+    private let dontHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        let attributedTitle = NSMutableAttributedString(string: "Don't have an account? ", attributes: [.font: UIFont.systemFont(ofSize: 16),
+                                                                                                        .foregroundColor: UIColor.white])
+        
+        attributedTitle.append(NSMutableAttributedString(string: "Sign Up", attributes: [.font: UIFont.boldSystemFont(ofSize: 16),
+                                                                                         .foregroundColor: UIColor.white]))
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
+        
         return button
     }()
     
@@ -57,6 +69,13 @@ class LoginController: UIViewController {
         super.viewDidLoad()
         
         configureUI()
+    }
+    
+    // MARK: - Selecters
+    
+    @objc func handleShowSignUp() {
+        let controller = RegisterationController()
+        navigationController?.pushViewController(controller, animated: true)
     }
     
     // MARK: - Helpers
@@ -85,8 +104,13 @@ class LoginController: UIViewController {
                      paddingTop: 32,
                      paddingLeft: 32,
                      paddingRight: 32)
+        
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.anchor(left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor, paddingLeft: 32, paddingRight: 32)
     }
-    
+}
+
+extension UIViewController {
     func configureGradientLayer() {
         let gradient = CAGradientLayer()
         gradient.colors = [UIColor.systemPurple.cgColor, UIColor.systemPink.cgColor]

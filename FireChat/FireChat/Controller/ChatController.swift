b4c8b8aa550582096c541ae28a9c7ea5778f23d3
1000JI <chjh1992@gmail.com>
2020-06-15 11:25:38 +0900

    feat: Custom Input Accessory View For Sending Messages

--- FireChat/FireChat/Controller/ChatController.swift
+++ FireChat/FireChat/Controller/ChatController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
+private let reuseIdentifier = "MessageCell"
+
 class ChatController: UICollectionViewController {
     
     // MARK: - Properties
     
     private let user: User
+    
+    private lazy var customInputView: CustomInputAccessoryView = {
+        let iv = CustomInputAccessoryView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 50))
+        return iv
+    }()
     
     // MARK: - LifeCycle
     
@@ -30,9 +37,46 @@ class ChatController: UICollectionViewController {
         configureUI()
     }
     
+    override var inputAccessoryView: UIView? {
+        get { return customInputView }
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
     // MARK: - Helpers
     
     func configureUI() {
         collectionView.backgroundColor = .white
+        configureNavigationBar(withTitle: user.username, prefersLargeTitles: false)
+        
+        collectionView.register(MessageCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView.alwaysBounceVertical = true // 세로 스크롤이 내용의 끝에 도달 할 때, 항상 수신 거부(?)가 발생하는지 여부를 결정하는 값
+    }
+}
+
+// MARK: - CollectionView DataSource
+
+extension ChatController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! MessageCell
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension ChatController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 16, left: 0, bottom: 16, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
     }
 }

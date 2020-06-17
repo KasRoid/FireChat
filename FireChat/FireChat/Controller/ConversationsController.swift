1926597c1f19ee156a3449cc688743f5b745089f
1000JI <chjh1992@gmail.com>
2020-06-17 21:12:57 +0900

    feat: User Profile UI

--- FireChat/FireChat/Controller/ConversationsController.swift
+++ FireChat/FireChat/Controller/ConversationsController.swift
@@ -48,7 +48,10 @@ class ConversationsController: UIViewController {
     // MARK: - Selectors
     
     @objc func showProfile() {
-        logout()
+        let controller = ProfileController()
+        let nav = UINavigationController(rootViewController: controller)
+        nav.modalPresentationStyle = .fullScreen
+        present(nav, animated: true)
     }
     
     @objc func showNewMessage() {

f3c679e052099482546d0bec0bd92b2247b9f0bb
1000JI <chjh1992@gmail.com>
2020-06-20 03:16:16 +0900

    feat: Searching For Users

--- FireChat/FireChat/Controller/NewMessageController.swift
+++ FireChat/FireChat/Controller/NewMessageController.swift
@@ -19,7 +19,14 @@ class NewMessageController: UITableViewController {
     // MARK: - Properties
     
     private var users = [User]()
+    private var filteredUsers = [User]()
     weak var delegate: NewMessageControllerDelegate?
+    private let searchController = UISearchController(searchResultsController: nil)
+    
+    private var isSearchMode: Bool {
+        return searchController.isActive &&
+            !searchController.searchBar.text!.isEmpty
+    }
     
     // MARK: - Lifecycle
     
@@ -27,6 +34,7 @@ class NewMessageController: UITableViewController {
         super.viewDidLoad()
         
         configureUI()
+        configureSearchController()
         fetchUsers()
     }
     
@@ -55,20 +63,34 @@ class NewMessageController: UITableViewController {
         tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
         tableView.rowHeight = 80
     }
+    
+    func configureSearchController() {
+        searchController.searchResultsUpdater = self // ?
+        searchController.searchBar.showsCancelButton = false // ?
+        navigationItem.searchController = searchController
+        searchController.obscuresBackgroundDuringPresentation = false // ?
+        searchController.hidesNavigationBarDuringPresentation = false // ?
+        searchController.searchBar.placeholder = "Search for a user"
+        definesPresentationContext = false // ?
+        
+        if let textField = searchController.searchBar.value(forKey: "searchField") as? UITextField {
+            textField.textColor = .systemPurple
+            textField.backgroundColor = .white
+        }
+    }
+    
 }
 
 // MARK: - UITableViewDataSource
 
 extension NewMessageController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return users.count
+        return isSearchMode ? filteredUsers.count : users.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
-        
-        cell.user = users[indexPath.row]
-        
+        cell.user = isSearchMode ? filteredUsers[indexPath.row] : users[indexPath.row]
         return cell
     }
 }
@@ -77,6 +99,22 @@ extension NewMessageController {
 
 extension NewMessageController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        delegate?.controller(self, wantsToStartChatWith: users[indexPath.row])
+        let user = isSearchMode ? filteredUsers[indexPath.row] : users[indexPath.row]
+        delegate?.controller(self, wantsToStartChatWith: user)
+    }
+}
+
+// MARK: - UISearchResultsUpdating
+
+extension NewMessageController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let searchText = searchController.searchBar.text?.lowercased() else { return }
+        
+        filteredUsers = users.filter({ user -> Bool in
+            return user.username.contains(searchText) || user.fullname.contains(searchText)
+        })
+        // "so joong".contains("so ") => true
+        
+        self.tableView.reloadData()
     }
 }
